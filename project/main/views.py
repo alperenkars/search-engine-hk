75b79ac8d1186ab13619d6deaae53f5e38204873
Anthonykounmpo <chanhoyeung0216@gmail.com>
2025-05-05 19:15:02 +0800

    restructured the code in views.py

--- project/main/views.py
+++ project/main/views.py
@@ -12,103 +12,84 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
+def get_stemmed_keywords():
+    """Get all stemmed keywords from the database."""
+    retrieval = Retrieval("main.db")
+    return [word for word in retrieval.get_all_keywords() if word != '']
+
+def get_user_query_history(cookie_id):
+    """Get query history for a specific user."""
+    try:
+        query_history = EachUserQueryHistory.objects.get(cookie_id=cookie_id)
+        return UserQuery.objects.filter(history=query_history).order_by('-timestamp')[:50]
+    except EachUserQueryHistory.DoesNotExist:
+        return []
+
+def save_query_to_history(query_history, query_string):
+    """Save a new query to the user's history."""
+    if bool(query_string.strip()):
+        new_query = UserQuery(history=query_history, query=query_string)
+        new_query.save()
+
+        # Keep only the latest 10 queries
+        if UserQuery.objects.filter(history=query_history).count() > 10:
+            UserQuery.objects.filter(
+                pk__in=UserQuery.objects.filter(history=query_history)
+                .order_by('-timestamp')
+                .values_list('pk', flat=True)[10:]
+            ).delete()
+
 @csrf_protect
 def index(request):
-    # return HttpResponse("Hello world. You are currently at the main's index.")
-
     query_submitted = False
     query_results = []
-    stem_keywords = []
-
-    # Load stopwords from file
-    with open('stopwords.txt', 'r') as f:
-        stopwords = set(line.strip() for line in f)
-    stem_keywords = [word for word in stopwords]
+    stem_keywords = get_stemmed_keywords()
 
     if request.method == 'GET':
         cookie_id = request.COOKIES.get('user_cookie_id')
 
-        # when this is the very first time the user use the browser to view the search engine we interface
+        # First time user
         if not cookie_id:
             cookie_id = uuid.uuid4()
-
-            # make sure each user has a unique query history
-            new_query_history, created = EachUserQueryHistory.objects.get_or_create(cookie_id = str(cookie_id))
+            new_query_history, created = EachUserQueryHistory.objects.get_or_create(cookie_id=str(cookie_id))
 
             response = render(request, 'index.html', {
                 "message": "Since this is the first time you use our search engine, we will help you to set up a new cookie.",
                 "query_submitted": query_submitted,
                 "stem_keywords": stem_keywords
             })
-            # set the max_age to be 2 years, or 63072000 seconds
             response.set_cookie('user_cookie_id', str(cookie_id), max_age=63072000)
-
             return response
         
-        # when this is not the very first time the user user our search engine, then the user already has a cookie
-        else:
-            query_history = EachUserQueryHistory.objects.get(cookie_id = cookie_id)
-            queries = UserQuery.objects.filter(history = query_history).order_by('-timestamp')[:50]
-
-            return render(request, 'index.html', {
-                "queries": queries,
-                "query_submitted": query_submitted,
-                "stem_keywords": stem_keywords
-            })
+        # Returning user
+        queries = get_user_query_history(cookie_id)
+        return render(request, 'index.html', {
+            "queries": queries,
+            "query_submitted": query_submitted,
+            "stem_keywords": stem_keywords
+        })
         
     elif request.method == 'POST':
         cookie_id = request.COOKIES.get('user_cookie_id')
 
-        if cookie_id:
-            query_submitted = True
-            query_history = EachUserQueryHistory.objects.get(cookie_id = cookie_id)
+        if not cookie_id:
+            return HttpResponse("Sorry, we cannot find your cookie. Please enable your cookie.", status=400)
 
-            if query_history.request:
-                query_submitted = True
-                query_strings = query_history.request_query
+        query_submitted = True
+        query_history = EachUserQueryHistory.objects.get(cookie_id=cookie_id)
 
-                retrieval = Retrieval("main.db")
-                query_results = retrieval.retrieve(query_strings)
-
-                query_exist = UserQuery.objects.filter(history = query_history, query = query_strings).exists()
-
-                if not query_exist and bool(query_strings.strip()):
-                    new_query = UserQuery(history = query_history, query = query_strings)
-                    new_query.save()
-
-                    # Note that we only give the latest 10 query history for users
-                    if UserQuery.objects.filter(history = query_history).count() > 10:
-                        UserQuery.objects.filter(pk__in=UserQuery.objects.filter(history = query_history).order_by('-timestamp').values_list('pk', flat = True)[10:]).delete()
-                
-                queries = UserQuery.objects.filter(history = query_history).order_by('-timestamp')[:50]
-
-                query_history.request = False
-                query_history.request_query = ""
-                query_history.save()
-
-                return render(request,"index.html", {
-                    "query_results": query_results,
-                    "queries": queries,
-                    "query_submitted": query_submitted,
-                    "stem_keywords": stem_keywords
-                })
-            
-            query_strings = request.POST.get('query', '')
-
+        # Handle stored query request
+        if query_history.request:
+            query_strings = query_history.request_query
             retrieval = Retrieval("main.db")
             query_results = retrieval.retrieve(query_strings)
 
-            query_exist = UserQuery.objects.filter(history = query_history, query = query_strings).exists()
+            save_query_to_history(query_history, query_strings)
+            queries = get_user_query_history(cookie_id)
 
-            if not query_exist and bool(query_strings.strip()):
-                new_query = UserQuery(history = query_history, query = query_strings)
-                new_query.save()
-
-                # Note that we only give the latest 10 query history for users
-                if UserQuery.objects.filter(history = query_history).count() > 10:
-                    UserQuery.objects.filter(pk__in=UserQuery.objects.filter(history = query_history).order_by('-timestamp').values_list('pk', flat = True)[10:]).delete()
-
-            queries = UserQuery.objects.filter(history = query_history).order_by('-timestamp')[:50]
+            query_history.request = False
+            query_history.request_query = ""
+            query_history.save()
 
             return render(request, "index.html", {
                 "query_results": query_results,
@@ -117,8 +98,20 @@ def index(request):
                 "stem_keywords": stem_keywords
             })
         
-        else:
-            return HttpResponse("Sorry, we cannot find your cookie. Please enable your cookie.", status = 400)
+        # Handle new query request
+        query_strings = request.POST.get('query', '')
+        retrieval = Retrieval("main.db")
+        query_results = retrieval.retrieve(query_strings)
+
+        save_query_to_history(query_history, query_strings)
+        queries = get_user_query_history(cookie_id)
+
+        return render(request, "index.html", {
+            "query_results": query_results,
+            "queries": queries,
+            "query_submitted": query_submitted,
+            "stem_keywords": stem_keywords
+        })
 
 @csrf_protect
 def clear_history(request):
